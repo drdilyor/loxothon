af86d39a822acc5d3e750d87c1850afe3ab30aa2
drdilyor <drdilyor@outlook.com>
2021-07-20 22:18:55 +0500

    5.4 A (Not Very) Pretty Printer

--- lox/__init__.py
+++ lox/__init__.py
@@ -1,5 +1,6 @@
-from lox import expr, lox, scanner, token
+from lox import expr, lox, printer, scanner, token
 from lox.lox import *
+from lox.printer import *
 from lox.scanner import *
 from lox.token import *
 
@@ -7,5 +8,6 @@ __all__ = (
     lox .__all__
     + scanner.__all__
     + token.__all__
+    + printer.__all__
     + ['expr']
 )
---
+++ lox/printer.py
@@ -0,0 +1,36 @@
+import lox.expr as expr
+
+class AstPrinter(expr.Visitor[str]):
+    def print(self, e: expr.Expr):
+        return e.accept(self)
+
+    def parens(self, name: str, *exprs: expr.Expr) -> str:
+        return f'({name} {" ".join(i.accept(self) for i in exprs)})'
+
+    def visitBinaryExpr(self, e: expr.Binary) -> str:
+        return self.parens(e.operator.lexeme, e.left, e.right)
+
+    def visitGroupingExpr(self, e: expr.Grouping) -> str:
+        return self.parens('group', e.expression)
+
+    def visitLiteralExpr(self, e: expr.Literal) -> str:
+        return 'nil' if e.value is None else str(e.value)
+    
+    def visitUnaryExpr(self, e: expr.Unary) -> str:
+        return self.parens(e.operator.lexeme, e.right)
+
+
+__all__ = ['AstPrinter']
+
+if __name__ == '__main__':
+    from lox.token import Token, TokenType
+    print(AstPrinter().print(
+        expr.Binary(
+            expr.Unary(
+                Token(TokenType.MINUS, '-', None, 1),
+                expr.Literal(123)),
+            Token(TokenType.STAR, '*', None, 1),
+            expr.Grouping(
+                expr.Literal(45.67)))))
+
+
