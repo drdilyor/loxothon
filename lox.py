b793b155160cdf35b1591e0e60ab8e6861767de5
drdilyor <drdilyor@outlook.com>
2021-07-20 07:12:10 +0500

    4.1 The Interpreter Framework

---
+++ lox.py
@@ -0,0 +1,37 @@
+import sys
+
+
+had_error = False
+
+def run_file(path: str) -> None:
+    with open(path) as f:
+        code = f.read()
+    run(code)
+    if had_error:
+        sys.exit(65)
+
+
+def run_prompt() -> None:
+    global had_error
+    while True:
+        print(end='> ')
+        run(input())
+        had_error = False
+
+
+def run(source) -> None:
+    scanner = Scanner(source)
+    tokens = scanner.scanTokens()
+
+    for token in tokens:
+        println(token)
+
+
+def error(line: int, message: str) -> None:
+    report(line, '', message)
+
+
+def report(line: int, where: str, message: str) -> None:
+    global had_error
+    print(f'[line {line}] Error{where}: {message}')
+    had_error = True
---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+import lox
+
+def main(file = None, *rest):
+    if rest:
+        print('Usage: python lox.py [script]')
+        sys.exit(64)
+    elif file:
+        lox.run_file(file)
+    else:
+        lox.run_prompt()
+
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
