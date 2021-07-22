21841c72fedd1b9ecfcc317cf459e9783b1f99be
drdilyor <drdilyor@outlook.com>
2021-07-22 17:40:27 +0500

    8 New Test Runner support for Errors

--- test/interpreter/test_files.py
+++ test/interpreter/test_files.py
@@ -3,7 +3,11 @@ from pathlib import Path
 
 import pytest
 
-from lox import Parser, Interpreter, Scanner
+from lox import Parser, Interpreter, Scanner, lox
+
+expect_error = object()
+expect_runtime_error = object()
+
 
 def gather_tests():
     cdir = Path(__file__).parent.absolute()
@@ -12,9 +16,14 @@ def gather_tests():
     for file in cdir.glob('**/*.lox'):
         with open(file) as f:
             source = f.read()
-        expects = re.findall(r'print .*;\s*//\s*expect: (.*)', source)
-        expect = ''.join(f'{i}\n' for i in expects)
-        results.append((source, expect))
+        if source.startswith('// expect: runtime-error'):
+            results.append((source, expect_runtime_error))
+        elif source.startswith('// expect: error'):
+            results.append((source, expect_error))
+        else:
+            expects = re.findall(r'print .*;\s*//\s*expect: (.*)', source)
+            expect = ''.join(f'{i}\n' for i in expects)
+            results.append((source, expect))
 
     return results
 
@@ -22,5 +31,11 @@ def gather_tests():
 @pytest.mark.parametrize('s,expect', gather_tests())
 def test_interpreter(s, expect, capsys):
     Interpreter().interpret(Parser(Scanner(s).scan_tokens()).parse())
-    assert capsys.readouterr().out == expect
+    if expect is expect_runtime_error:
+        assert lox.had_runtime_error
+    elif expect is expect_error:
+        assert lox.had_error
+    else:
+        assert capsys.readouterr().out == expect
+
 
